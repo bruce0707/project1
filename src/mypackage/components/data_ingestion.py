f6f58ff93161eedb9bbee47d187678c7d693a4fa
bruce0707 <ankushhumai63@gmail.com>
2026-03-20 11:45:04 +0530

    transformation of the datasets

--- src/mypackage/components/data_ingestion.py
+++ src/mypackage/components/data_ingestion.py
@@ -4,6 +4,9 @@ from mypackage.logger import logging
 from mypackage.exception import CustomException
 from dataclasses import dataclass
 import pandas as pd
+from mypackage.components.data_transformation import DataTransformation
+from mypackage.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig: 
     raw_data_path: str = os.path.join(os.getcwd(),'artifacts','raw_data.csv')
@@ -47,5 +50,11 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()             
-
+    obj.initiate_data_transformation(
+        "artifacts/train_data.csv",
+        "artifacts/test_data.csv"
+    )
+    train_data, test_data = obj.initiate_data_ingestion()             
+     
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
